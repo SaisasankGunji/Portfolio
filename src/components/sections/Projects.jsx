0de52ce4dcec770bc951cbc843715a8289cc0a22
SaisasankGunji <saisasnk.gunji@gmail.com>
2026-04-01 22:50:14 +0530

    Added new project

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -61,6 +61,55 @@ const Projects = () => {
             </div>
 
             <div className="flex flex-col justify-between p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]">
+              <h3 className="text-xl font-bold mb-2">AI Resume Analyzer</h3>
+              <p className="text-gray-400 mb-4">
+                AI Resume Analyzer is a full-stack MERN application that
+                evaluates resumes against job descriptions using AI. Users can
+                upload PDF resumes and receive an ATS score, missing keywords,
+                and personalized suggestions. Integrated Cohere AI for
+                intelligent analysis, Firebase for Google authentication, and
+                deployed on AWS EC2 for scalability.
+              </p>
+
+              <div className="flex flex-wrap gap-2">
+                {[
+                  "React",
+                  "MongoDB",
+                  "Node.js",
+                  "ExpressJS",
+                  "Cohere AI",
+                  "Firebase Auth",
+                  "AWS EC2",
+                ].map((tech, id) => (
+                  <span
+                    key={id}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+        hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex flex-col gap-2 mt-4">
+                <a
+                  href="http://16.171.254.68:8000/"
+                  className="text-blue-400 hover:text-blue-300 transition-colors"
+                >
+                  Live Project
+                  <span className="mx-2 font-extrabold">&#8594;</span>
+                </a>
+                <a
+                  href="https://github.com/SaisasankGunji/AI-resume-analyzer"
+                  className="text-blue-400 hover:text-blue-300 transition-colors"
+                >
+                  GitHub Repository
+                  <span className="mx-2 font-extrabold">&#8594;</span>
+                </a>
+              </div>
+            </div>
+
+            <div className="flex flex-col justify-between p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]">
               <h3 className="text-xl font-bold mb-2">Expenses Tracker</h3>
               <h4 className="text-md font-bold my-2">
                 Led the frontend development
@@ -182,7 +231,7 @@ const Projects = () => {
                     >
                       {tech}
                     </span>
-                  )
+                  ),
                 )}
               </div>
 
